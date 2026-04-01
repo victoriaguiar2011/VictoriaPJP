1801003ad56596b13dd74e9ebd79c8b40c478d6f
VICTORIA AGUIAR <victoria62211316@alunos.sc.senac.br>
2026-03-31 21:15:54 -0300

    Laços de Repetição

---
+++ LR.py
@@ -0,0 +1,20 @@
+total_compras = 0
+quantidade = 0
+
+valor = float(input("Informe o valor da compra (0 para encerrar): "))
+
+while valor != 0:
+    total_compras += valor 
+    quantidade += 1
+
+    valor = float(input("Digite o valor da compra (0 para encerrar): "))
+
+if quantidade > 0:
+    media = total_compras / quantidade
+else:
+    media = 0
+
+print(f"Total das compras: R$ {total_compras:.2f}")
+print(f"Quantidade de compras: {quantidade}")
+print(f"Valor médio das compras: R$ {media:.2f}")
+
