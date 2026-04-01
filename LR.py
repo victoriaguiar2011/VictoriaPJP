060ec2149db89886cfd0008817788be69aeb2848
VICTORIA AGUIAR <victoria62211316@alunos.sc.senac.br>
2026-03-31 21:29:15 -0300

    Laços de Repetição

--- LR.py
+++ LR.py
@@ -1,3 +1,4 @@
+#Forma que eu tentei fazer e deu certo
 total_compras = 0
 quantidade = 0
 
@@ -11,8 +12,28 @@ while valor != 0:
 
 if quantidade > 0:
     media_compras = total_compras / quantidade
+else:
+    media_compras = 0
 
 print(f"Total das compras: R$ {total_compras:.2f}")
 print(f"Quantidade de compras: {quantidade}")
 print(f"Valor médio das compras: R$ {media_compras:.2f}")
 
+#Explicação do Vlad
+total_compras = 0
+quantidade = 0
+valor = ""
+
+while valor != 0:
+    valor = float(input("Digite o valor da compra (0 para encerrar): "))
+    total_compras += valor 
+    quantidade += 1
+
+if quantidade > 0:
+    media_compras = total_compras / quantidade
+else:
+    media_compras = 0
+
+print(f"Total das compras: R$ {total_compras:.2f}")
+print(f"Quantidade de compras: {quantidade}")
+print(f"Valor médio das compras: R$ {media_compras:.2f}")
