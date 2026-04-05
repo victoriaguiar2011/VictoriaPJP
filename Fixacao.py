babc2d59b77693668b8b3bd1aa6be2a7fa5c920b
Victoria <victoriaguiardesign@gmail.com>
2026-04-05 11:03:57 -0300

    Exercícios livres de fixação

---
+++ Fixacao.py
@@ -0,0 +1,51 @@
+#tentativa sozinha (+= e -=)
+pagamento = 1700
+aluguel = 800
+comida = 250
+internet = 100
+
+loteria = 3000
+pagamento += loteria
+
+pagamento -= (aluguel + comida + internet)
+print(f"{pagamento}")
+
+#organizando o código com ajuda da IA (+= e -=)
+pagamento = 1700
+loteria = 3000
+
+aluguel = 800
+comida = 250
+internet = 100
+
+saldo = pagamento + loteria
+gastos = (aluguel + comida + internet)
+
+saldo -= gastos
+print(saldo)
+
+#multiplicar ( *= )
+
+valor_inicial = 2000
+valor_inicial *= 2
+valor_inicial *= 1.5
+saldo = valor_inicial
+
+gasto = 300
+saldo -= gasto
+
+print(saldo)
+
+#multiplicar organizado pela IA ( *= )
+
+valor_inicial = 2000
+saldo = valor_inicial
+
+#ganhos
+saldo *= 2
+saldo *= 1.5
+
+#gastos
+gasto = 300
+saldo -= gasto
+print(saldo)
